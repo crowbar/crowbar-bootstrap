0ef5cef53599470bdf6f02ee831be31385847766
Maximilian Meister <mmeister@suse.de>
2016-08-01 10:02:42 +0200

    fix path to application

--- config/puma.rb
+++ config/puma.rb
@@ -1,4 +1,4 @@
-ROOT = File.expand_path("..", __FILE__)
+ROOT = File.expand_path("../..", __FILE__)
 ENVIRONMENT = ENV["CROWBAR_INIT_ENV"] || "production"
 
 THREADS = ENV["CROWBAR_INIT_THREADS"] || 10
